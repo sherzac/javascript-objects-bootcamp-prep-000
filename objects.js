a03896c9975996a09ba147ba1bcd63c1d309c4c6
Shaun Rezac <shaun.rezac@gmail.com>
2018-05-09 14:49:58 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -7,6 +7,10 @@ function updatePlaylist(playlist, artistName, songTitle) {
 }
 
 function removeFromPlaylist(playlist, artistName) {
+<<<<<<< HEAD
   delete playlist[artistName]
+=======
+  delete playlist.artistName
+>>>>>>> 98aa2df34635fdfec148b75444b427dac3737e7e
   return playlist
 }
